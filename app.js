e2ecf8f0d0864b4cad88b1f3a5115a1c28e93352
Roger Cores <rogercores2@gmail.com>
2016-08-01 23:36:08 +0530

    Minor Bugs Solved

--- app.js
+++ app.js
@@ -68,8 +68,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     console.log(err);
-    res.status(err.status || 500);
-    res.render('error', {
+    res.status(err.status || 500).send({
       message: err.message,
       error: err
     });
